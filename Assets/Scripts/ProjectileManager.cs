6b9484636211f6829dac6f8e4f2658fab96a5e76
Danny North <lds.north@gmail.com>
2017-05-07 19:24:35 -0600

    Projectiles fire on single grip press

---
+++ Assets/Scripts/ProjectileManager.cs
@@ -0,0 +1,31 @@
+﻿using DigitalRuby.LightningBolt;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+using Assets.Scripts.Extensions;
+
+public class ProjectileManager : MonoBehaviour
+{
+
+    public ParticleSystem projectilePrefab;
+
+    private VRTK_ControllerEvents controllerEvents;
+    private GameObject projectile;
+
+    void Awake()
+    {
+        controllerEvents = GetComponent<VRTK_ControllerEvents>();
+
+        controllerEvents.GripPressed += OnGripPressed;
+    }
+
+    public void OnGripPressed(object sender, ControllerInteractionEventArgs e)
+    {
+        var projectile = Instantiate(projectilePrefab, transform.position, transform.rotation);
+        Destroy(projectile.gameObject, 2f);
+    }
+}
+

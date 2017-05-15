ee38e47ece9e06798282f86e0ac6ecdb480de6ad
Danny North <lds.north@gmail.com>
2017-05-14 23:45:55 -0600

    Skeletons move and rotate towards player

--- Assets/Scripts/SkeletonController.cs
+++ Assets/Scripts/SkeletonController.cs
@@ -5,23 +5,68 @@ using VRTK;
 
 public class SkeletonController : MonoBehaviour {
 
-    public float speed = 3f;
+    [Tooltip("The target being attacked. If not set, defaults to the HMD.")]
+    public Transform target;
 
-    private Transform headset;
+    public float walkingSpeed = 3f;
+    public float turningSpeed = 100f;
+    public float attackingDistance = 1f;
+
     private Animator animator;
 
 	// Use this for initialization
 	void Start () {
         animator = GetComponent<Animator>();
-        headset = VRTK_DeviceFinder.HeadsetCamera();
 
-        animator.SetBool("isWalking", true);
+        if(target == null)
+            target = VRTK_DeviceFinder.HeadsetCamera();
+
     }
 
     // Update is called once per frame
     void Update () {
-        float step = speed * Time.deltaTime;
-        var headsetPosNoVertical = new Vector3(headset.position.x, transform.position.y, headset.position.z);
-        transform.position = Vector3.MoveTowards(transform.position, headsetPosNoVertical, step);
+        TurnTowards(target);
+
+        Debug.Log("Is move allowed: " + IsMoveAllowed());
+        if(IsMoveAllowed() && !IsInAttackingDistance())
+        {
+            WalkTowards(target);
+        }
+        else
+        {
+            Attack(target);
+        }
+    }
+
+    private void WalkTowards(Transform target)
+    {
+        animator.SetBool("isWalking", true);
+        float walkingStep = walkingSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, GetNonVerticalTargetPos(target), walkingStep);
+    }
+
+    private void TurnTowards(Transform target)
+    {
+        transform.LookAt(GetNonVerticalTargetPos(target));
+    }
+
+    private void Attack(Transform target)
+    {
+        animator.SetTrigger("attack");
+    }
+
+    private bool IsMoveAllowed()
+    {
+        return !animator.GetBool("attack");
+    }
+
+    private bool IsInAttackingDistance()
+    {
+        return Vector3.Distance(transform.position, GetNonVerticalTargetPos(target)) <= attackingDistance;
+    }
+
+    private Vector3 GetNonVerticalTargetPos(Transform target)
+    {
+        return new Vector3(target.position.x, transform.position.y, target.position.z);
     }
 }
